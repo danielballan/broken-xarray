1030891760b99eefaf933a1a92e62786fc247cfc
Dan Allan <dallan@bnl.gov>
2019-09-13 11:59:23 -0400

    typos

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ local = open_catalog('catalog.yml')
 remote = open_catalog('intake://localhost:5000')
 uid = '9b24c09f-ef87-4b43-af1c-e89c7e29751e'
 remote_run = remote.filled_example[uid]()
-remote_ds = rrun.primary.to_dask()
+remote_ds = remote_run.primary.to_dask()
 remote_ds['fccd_image'].compute()  # raises ValueError
